7fcbf6e95328272b9d38a79bb19fa9fe1d0fd14d
Andres <GustavoAndres>
2021-06-30 09:05:14 -0500

    Edit line of create article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,7 +24,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    @article.user=current_user
+    @article.user_id = current_user.id
 
     if @article.save
       redirect_to @article
