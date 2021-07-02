c8a17c2a02485c49ea2e3abbb0bef2cdc05fe7ef
Andres <GustavoAndres>
2021-07-02 14:45:17 -0500

    add relations in user and create user controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,39 @@
+class UsersController < ApplicationController
+
+  def index
+    @users =  User.all
+  end
+
+  def show
+    puts params
+    @user = User.find(params[:id])
+    @articles = @user.articles
+    user_path(@user)
+  end
+
+  def following
+    @title = "Following"
+    @users =  current_user.followings
+    render 'users/show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @users =  current_user.followers
+    render 'users/show_follow'
+  end
+
+  def follow
+    follow = Follow.new(follower_id: current_user.id, followed_user_id: params[:id])
+    follow.save
+    redirect_back(fallback_location: root_path)
+  end
+
+  def unfollow
+    puts "--" * 20
+    follow = Follow.where(follower_id: current_user.id, followed_user_id: params[:id])
+    puts follow[0].destroy
+    redirect_back(fallback_location: root_path)
+  end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,22 @@ class User < ApplicationRecord
   has_many :articles, dependent: :destroy
   has_many :comments, dependent: :destroy
 
+  # Will return an array of follows for the given user instance
+  # Devolverá un array de seguimientos para la instancia de usuario dada
+  has_many :received_follows, foreign_key: :followed_user_id, class_name: "Follow"
+
+  # Will return an array of users who follow the user instance
+  # Devolverá un array de usuarios que siguen a la instancia de usuario
+  has_many :followers, through: :received_follows, source: :follower
+
+   #####################
+
+  # returns an array of follows a user gave to someone else}
+  # Devuelve un array de seguimientos que un usuario dio a otra persona
+  has_many :given_follows, foreign_key: :follower_id, class_name: "Follow"
+
+  # returns an array of other users who the user has followed
+  # devuelve un array de otros usuarios a los que el usuario ha seguido
+  has_many :followings, through: :given_follows, source: :followed_user
+
 end
